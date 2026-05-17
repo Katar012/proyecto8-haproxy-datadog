dfd74f69ec17c724ba43a1b0a68da47e9d20c97c
jvilamarin31 <juan.vila@uao.edu.co>
2026-05-17 17:49:08 -0500

    Nueva maquina virtual, servicio sftp

--- backends/app.py
+++ backends/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template, jsonify
 import os
 import socket
+import paramiko
+import tempfile
 
 app = Flask(__name__)
 
@@ -8,6 +10,13 @@ UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 node_id = os.getenv("NODE_ID", socket.gethostname())
+SFTP_HOST = "192.168.65.20"
+SFTP_PORT = 22
+
+SFTP_USER = "sftpuser"
+SFTP_PASS = "1234"
+
+REMOTE_DIR = "/uploads"
 
 @app.route("/")
 def index():
@@ -24,14 +33,45 @@ def upload():
     if file.filename == "":
         return jsonify({"error": "empty filename"}), 400
 
-    filepath = os.path.join(UPLOAD_FOLDER, file.filename)
-    file.save(filepath)
+    try:
 
-    return jsonify({
-        "status": "uploaded",
-        "node": node_id,
-        "filename": file.filename
-    })
+        # Guardar temporal local
+        temp = tempfile.NamedTemporaryFile(delete=False)
+
+        file.save(temp.name)
+
+        # Conexión SFTP
+        transport = paramiko.Transport((SFTP_HOST, SFTP_PORT))
+
+        transport.connect(
+            username=SFTP_USER,
+            password=SFTP_PASS
+        )
+
+        sftp = paramiko.SFTPClient.from_transport(transport)
+        remote_path = f"{REMOTE_DIR}/{file.filename}"
+
+        # Upload remoto
+        sftp.put(temp.name, remote_path)
+
+        # Cerrar conexión
+        sftp.close()
+        transport.close()
+
+        # Eliminar temporal
+        os.remove(temp.name)
+
+        return jsonify({
+            "status": "uploaded",
+            "node": node_id,
+            "remote_path": remote_path
+        })
+
+    except Exception as e:
+
+        return jsonify({
+            "error": str(e)
+        }), 500
 
 @app.route("/files")
 def files():
