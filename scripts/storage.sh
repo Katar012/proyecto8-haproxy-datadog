cad7d63528a9f99333dc54c4bc9412280c24f10e
jvilamarin31 <juan.vila@uao.edu.co>
2026-05-18 05:39:11 -0500

    Modularidad de scripts

--- scripts/storage.sh
+++ scripts/storage.sh
@@ -1,1 +1,24 @@
+#!/bin/bash
 
+apt update
+
+apt install -y \
+  openssh-server \
+  nginx
+
+systemctl enable ssh
+systemctl restart ssh
+
+systemctl enable nginx
+systemctl restart nginx
+
+mkdir -p /sftp/uploads
+
+useradd -m sftpuser
+
+echo "sftpuser:1234" | chpasswd
+
+chown root:root /sftp
+chmod 755 /sftp
+
+chown sftpuser:sftpuser /sftp/uploads
